4bf2ae40dfd97980d64eb439ccc007897ccb4e4d
Кирилл Коннов <k-konnov@list.ru>
2022-01-20 20:54:10 +0400

    Added base entities

---
+++ PopIT.FoodOrder.Backend/FoodOrder.Domain/Entities/Beverage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FoodOrder.Domain
+{
+	public class Beverage
+	{
+		public Guid Id { get; set; }
+		public string Name { get; set; }
+		public decimal Price { get; set; }
+
+		public IEnumerable<Order> Orders { get; set; }
+	}
+}
