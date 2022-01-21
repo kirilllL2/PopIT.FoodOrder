d6c4bc7aa7b384c52b4278415aa5c7e4aee405ad
Кирилл Коннов <k-konnov@list.ru>
2022-01-21 12:40:46 +0400

    Added Exceptions

---
+++ PopIT.FoodOrder.Backend/FoodOrder.Application/Common/Exceptions/EntityIdNotFoundException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodOrder.Application.Common.Exceptions
+{
+	public class EntityIdNotFoundException : FoodOrderException
+	{
+        public EntityIdNotFoundException(string modelName, int id) =>
+            (ModelName, Id) = (modelName, id);
+        
+        public string ModelName { get; }
+        public int Id { get; }
+        public override string Message => $"Entity {ModelName} with id {Id} not found";
+    }
+}
