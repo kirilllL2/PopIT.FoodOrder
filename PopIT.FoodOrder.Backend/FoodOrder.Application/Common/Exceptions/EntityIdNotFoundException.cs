8f2c3465af2edf8cd9f60abe72e555def67dd0f9
Кирилл Коннов <k-konnov@list.ru>
2022-01-21 12:44:08 +0400

    Fix type Id

--- PopIT.FoodOrder.Backend/FoodOrder.Application/Common/Exceptions/EntityIdNotFoundException.cs
+++ PopIT.FoodOrder.Backend/FoodOrder.Application/Common/Exceptions/EntityIdNotFoundException.cs
@@ -1,18 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FoodOrder.Application.Common.Exceptions
 {
 	public class EntityIdNotFoundException : FoodOrderException
 	{
-        public EntityIdNotFoundException(string modelName, int id) =>
+        public EntityIdNotFoundException(string modelName, Guid id) =>
             (ModelName, Id) = (modelName, id);
         
         public string ModelName { get; }
-        public int Id { get; }
+        public Guid Id { get; }
         public override string Message => $"Entity {ModelName} with id {Id} not found";
     }
 }
