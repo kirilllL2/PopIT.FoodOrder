180c90482028859f87badd317573634ddabbd4bd
Кирилл Коннов <k-konnov@list.ru>
2022-01-21 21:05:06 +0400

    Added validate for Soup commands and queries

---
+++ PopIT.FoodOrder.Backend/FoodOrder.Application/Soups/Commands/DeleteSoup/DeleteSoupCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using System;
+
+namespace FoodOrder.Application.Soups.Commands.DeleteSoup
+{
+	public class DeleteSoupCommandValidator : AbstractValidator<DeleteSoupCommand>
+	{
+		public DeleteSoupCommandValidator()
+		{
+			RuleFor(c => c.Id)
+				.NotEqual(Guid.Empty).WithMessage("Идентификатор обязателен");
+		}
+	}
+}
