ea2f026b9963c327e0a60a037115e96ecd8bbc4e
Кирилл Коннов <k-konnov@list.ru>
2022-01-21 20:37:54 +0400

    Added validate for Beverage commands and queries

---
+++ PopIT.FoodOrder.Backend/FoodOrder.Application/Beverages/Commands/UpdateBeverage/UpdateBeverageCommandValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using System;
+
+namespace FoodOrder.Application.Beverages.Commands.UpdateBeverage
+{
+	public class UpdateBeverageCommandValidator : AbstractValidator<UpdateBeverageCommand>
+	{
+		public UpdateBeverageCommandValidator()
+		{
+			RuleFor(c => c.Id)
+				.NotEqual(Guid.Empty).WithMessage("Идентификатор обязателен");
+
+			RuleFor(c => c.Name)
+				.NotNull().WithMessage("Напиток не может быть null.")
+				.Length(2, 30).WithMessage("Название напитка должно быть от 2 до 30 символов.");
+
+			RuleFor(c => c.Price)
+				.GreaterThan(0).WithMessage("Цена должна быть больше нуля.");
+		}
+	}
+}
