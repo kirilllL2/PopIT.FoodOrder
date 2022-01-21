e151e847073b65b69286fafdc462cd97ae752046
Кирилл Коннов <k-konnov@list.ru>
2022-01-21 20:45:16 +0400

    Added validate for Garnish commands and queries

---
+++ PopIT.FoodOrder.Backend/FoodOrder.Application/Garnishes/Commands/DeleteGarnish/DeleteGarnishCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using System;
+
+namespace FoodOrder.Application.Garnishes.Commands.DeleteGarnish
+{
+	public class DeleteGarnishCommandValidator : AbstractValidator<DeleteGarnishCommand>
+	{
+		public DeleteGarnishCommandValidator()
+		{
+			RuleFor(c => c.Id)
+				.NotEqual(Guid.Empty).WithMessage("Идентификатор обязателен");
+		}
+	}
+}
---
+++ PopIT.FoodOrder.Backend/FoodOrder.Application/Garnishes/Commands/UpdateGarnish/UpdateGarnishCommandValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using System;
+
+namespace FoodOrder.Application.Garnishes.Commands.UpdateGarnish
+{
+	public class UpdateGarnishCommandValidator : AbstractValidator<UpdateGarnishCommand>
+	{
+		public UpdateGarnishCommandValidator()
+		{
+			RuleFor(c => c.Id)
+				.NotEqual(Guid.Empty).WithMessage("Идентификатор обязателен");
+
+			RuleFor(c => c.Name)
+				.NotNull().WithMessage("Гарнир не может быть null.")
+				.Length(2, 30).WithMessage("Название гарнира должно быть от 2 до 30 символов.");
+
+			RuleFor(c => c.Price)
+				.GreaterThan(0).WithMessage("Цена должна быть больше нуля.");
+		}
+	}
+}
