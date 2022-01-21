fa1b99e76d02740a42b7cb58b1f6f3e42440c688
Кирилл Коннов <k-konnov@list.ru>
2022-01-21 20:53:27 +0400

    Added validate for Meat commands and queries

---
+++ PopIT.FoodOrder.Backend/FoodOrder.Application/Meats/Commands/CreateMeat/CreateMeatCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace FoodOrder.Application.Meats.Commands.CreateMeat
+{
+	public class CreateMeatCommandValidator : AbstractValidator<CreateMeatCommand>
+	{
+		public CreateMeatCommandValidator()
+		{
+			RuleFor(c => c.Name)
+				.NotNull().WithMessage("Мясо не может быть null.")
+				.Length(2, 30).WithMessage("Название мяса должно быть от 2 до 30 символов.");
+
+			RuleFor(c => c.Price)
+				.GreaterThan(0).WithMessage("Цена должна быть больше нуля.");
+		}
+	}
+}
---
+++ PopIT.FoodOrder.Backend/FoodOrder.Application/Meats/Queries/GetMeatDetails/GetMeatDetailsQueryValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using System;
+
+namespace FoodOrder.Application.Meats.Queries.GetMeatDetails
+{
+	public class GetMeatDetailsQueryValidator : AbstractValidator<GetMeatDetailsQuery>
+	{
+		public GetMeatDetailsQueryValidator()
+		{
+			RuleFor(q => q.Id)
+				.NotEqual(Guid.Empty).WithMessage("Идентификатор обязателен");
+		}
+	}
+}
