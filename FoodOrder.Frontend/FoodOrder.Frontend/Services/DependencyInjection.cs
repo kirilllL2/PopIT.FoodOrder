5d493fdbb85ed4e292076fb0b53b55200da7812c
ffgh04@gmail.com <1337CurrentLife228>
2022-01-23 20:28:40 +0400

    Setting frontend

---
+++ FoodOrder.Frontend/FoodOrder.Frontend/Services/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace FoodOrder.Frontend.Services
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            // services.AddScoped(sp => new HttpClient() { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            return services;
+        }
+    }
+}
